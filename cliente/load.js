dbb4204b0b94e069bcd07ea7cc94800f9c4eb3eb
Facundo Vattino <facuvatti@gmail.com>
2025-10-17 00:32:34 +0000

    ordenando y creando modulo

--- cliente/load.js
+++ cliente/load.js
@@ -1,36 +1,10 @@
-async function httpRequest(event,url,endpoint,method,body) { // Es un handler para formularios
-    let options = {
-        method: method,
-        headers: {'Content-Type': 'application/json'}
-    };
-    if (event) {
-        let data = formResult(event);
-        options.body = JSON.stringify(data);
-    };
-    if (body) options.body = JSON.stringify(body);
-    let response = await fetch(url + endpoint, options)
-    try {let json = await response.json();return json;} catch(e) {console.log(e,response);}
-}
+import {httpRequest} from "../utlis.js";
+
 function makeRoom(e) {
     e.preventDefault();
     httpRequest(e,"http://localhost:3000/","students","POST");
 }
 
-function formResult(event) {
-    const form = event.target;
-    for(let element of form.elements) {
-        if(!["BUTTON","INPUT"].includes(element.tagName)) form.remove(element);
-    }
-    const formData = new FormData(form);
-    const data = {};
-    for (let [key, value] of formData.entries()) {
-        if (typeof value === 'string') value = value.toLowerCase();
-        data[key] = value.trim();
-    }
-    form.reset();
-    console.log("Resultado del formulario:", data);
-    return data 
-}
 window.addEventListener('DOMContentLoaded', () => {
     // Crear un objeto con los parámetros del URL
     const params = new URLSearchParams(window.location.search);
@@ -39,7 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const year = params.get('year');
     const division = params.get('division');
     const specialty = params.get('specialty');
-
     if (year) document.getElementById('year').value = year;
     if (division) document.getElementById('division').value = division;
     if (specialty) document.getElementById('specialty').value = specialty;
--- cliente/main.js
+++ cliente/main.js
@@ -1,19 +1,5 @@
-function insertToSelection(options,select=undefined) {
-    if (select == undefined) {
-        select = document.createElement("select");
-    }
-    options = options.map(option => Object.values(option)[0]);
-    for (let option of options) {
-        let op = document.createElement("option");
-        op.textContent = option;
-        op.value = option;
-        select.append(op);
-    }
-    if (select.options.length === 1) {
-        select.selectedIndex = 0;
-    }
-    return select;
-}
+import {formResult,httpRequest,capitalize} from "../utlis.js";
+
 function createForm(containerID,inputs=[["name","text"]],action="creating",add_method="append",insertBefore="",onlyOne=true) {
     if (document.querySelectorAll("."+action+"-"+containerID).length == 0 || !onlyOne) {
         let form = document.createElement("form");
@@ -59,38 +45,7 @@ function createForm(containerID,inputs=[["name","text"]],action="creating",add_m
         return form;
     }
 }
-function formResult(event) {
-    event.preventDefault();
-    const form = event.target;
-    for(let element of form.elements) {
-        if(!["BUTTON","INPUT"].includes(element.tagName)) form.remove(element);
-    }
-    const formData = new FormData(form);
-    const data = {};
-    for (let [key, value] of formData.entries()) {
-        if (typeof value === 'string') value = value.toLowerCase();
-        data[key] = value.trim();
-    }
-    form.reset();
-    console.log("Form Result:", data);
-    return data 
-}
-async function httpRequest(endpoint,method,body,url="http://localhost:3000/",event=null) { // Es un handler para formularios
-    let options = {
-        method: method,
-        headers: {'Content-Type': 'application/json'}
-    };
-    if (event) {
-        let data = formResult(event);
-        options.body = JSON.stringify(data);
-    };
-    if (body) options.body = JSON.stringify(body);
-    let response = await fetch(url + endpoint, options)
-    try {let json = await response.json();return json;} catch(e) {console.log(e,response);}
-}
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+
 function radioButton(event,row) {
     event.preventDefault();
     let button = event.currentTarget;
@@ -132,19 +87,6 @@ function makeRow(row,table) {
 
     if(table.typeof == "string") {table = document.getElementById(table);}
     table.append(tr);
-}
-function selected(select){
-    if(select.options.length === 1) return select.options[0];
-    else {
-        let selection = select.options[select.selectedIndex];
-        return selection;
-    }
-}
-async function  dbOptions(select,endpoint) {
-    options = await httpRequest(endpoint,"GET");
-    select = insertToSelection(options,select);
-    if (select.options.length === 1) select.dispatchEvent(new Event('change'));
-    return select;
 }
 
 function visibility(elements,hide,ids=false) {
